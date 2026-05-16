61bab4e947930c05dca1d4e909c96332b7b8d16c
majid <majidalaamri85@gmail.com>
2026-05-16 10:52:38 +0400

    Update template settings

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -49,21 +49,14 @@ ROOT_URLCONF = 'hotel_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'loaders': [
-                ('django.template.loaders.cached.Loader', [
-                    'django.template.loaders.filesystem.Loader',
-                    'django.template.loaders.app_directories.Loader',
-                ]),
-            ] if not DEBUG else None,
         }
     }
 ]
