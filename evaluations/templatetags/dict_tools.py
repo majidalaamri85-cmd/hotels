d4759969fe50508afdf9e76b297721c17b918be1
majid <majidalaamri85@gmail.com>
2026-05-16 16:59:43 +0400

    Show star rating icons using stars_html template filter

--- evaluations/templatetags/dict_tools.py
+++ evaluations/templatetags/dict_tools.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -8,3 +9,20 @@ def get_item(mapping, key):
     if not mapping:
         return None
     return mapping.get(key)
+
+
+@register.filter
+def stars_html(value):
+    """Render filled/empty star icons for a 1–5 integer rating."""
+    try:
+        n = int(value)
+    except (TypeError, ValueError):
+        return ''
+    filled = '★' * n
+    empty = '☆' * (5 - n)
+    return mark_safe(
+        f'<span class="stars-rating" aria-label="{n} نجوم">'
+        f'<span class="stars-filled">{filled}</span>'
+        f'<span class="stars-empty">{empty}</span>'
+        f'</span>'
+    )
